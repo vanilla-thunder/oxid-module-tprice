a10d2088de148d67dc00eacdac5ceb140561ac0b
Marat <m@marat.ws>
2015-08-24 16:39:09 +0200

    version 0.1.0

---
+++ application/views/admin/de/tprice_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * bestlife AG - enhanced presentation for TPrice (recommended retail price / old price) in OXID eShop
+ * Copyright (C) 2015  bestlife AG
+ * info:  oxid@bestlife.ag
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+ */
+
+$sLangName = 'Deutsch';
+$aLang     = array(
+    'charset'                    => 'UTF-8',
+    'ARTICLE_EXTEND_TPRICE'      => 'Statt-Preis',
+    'HELP_ARTICLE_EXTEND_BPRICE' => 'bei "Einführungspreis" wird der aktuelle Preis als Einführungspreis definiert und der Statt-Preis als "regulärer Preis"',
+    'BLA_TPRICE_RRP'             => 'UVP',
+    'BLA_TPRICE_EXRRP'           => 'ehem. UVP',
+    'BLA_TPRICE_OLD'             => 'alter Preis',
+    'BLA_TPRICE_REGULAR'         => 'regulärer Preis',
+    'BLA_TPRICE_INTRO'           => 'Einführungspreis',
+);
