a75e8e6943c7f646488c534bcbbcd9259f5fa2c6
vanilla thunder <vanilla-thunder@github.com>
2016-02-25 15:51:17 +0100

    fix

--- 3_publish.js
+++ 3_publish.js
@@ -1,13 +1,15 @@
 var runner = require('child_process');
 
-runner.exec("git add . && git commit -m 'updates' && git push", {cwd: './_master/'},
+var msg = ( process.argv[2] ? process.argv[2] : "updates");
+    
+runner.exec("git add . && git commit -m '"+msg+"' && git push", {cwd: './_master/'},
     function (err, stdout, stderr) {
         if(err) console.log(err);
         else if(stderr) console.log(stderr);
         else console.log("master branch updated");
     }
 );
-runner.exec("git add . && git commit -m 'updates' && git push", {cwd: './_module/'},
+runner.exec("git add . && git commit -m '"+msg+"' && git push", {cwd: './_module/'},
     function (err, stdout, stderr) {
         if(err) console.log(err);
         else if(stderr) console.log(stderr);
