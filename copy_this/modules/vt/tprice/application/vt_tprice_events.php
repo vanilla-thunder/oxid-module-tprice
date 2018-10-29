9fce1563b54cc67e764fc49f2a54975d0d81d4e3
vanilla-thunder <1874024+vanilla-thunder@users.noreply.github.com>
2018-10-29 11:55:26 +0100

    v6

---
+++ copy_this/modules/vt/tprice/application/vt_tprice_events.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * [vt] tprice
+ * Copyright (C) 2018  Marat Bedoev
+ * info:  schwarzarbyter@gmail.com
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * author: Marat Bedoev
+ */
+
+class vt_tprice_events extends oxSuperCfg
+{
+    public static function install()
+    {
+		$dbMetaDataHandler = oxNew(\OxidEsales\Eshop\Core\DbMetaDataHandler::class);
+
+		$table = 'oxarticles';
+		$field = 'VTTPRICETYPE';
+
+		if (!$dbMetaDataHandler->fieldExists($field, $table))
+		{
+			\OxidEsales\Eshop\Core\DatabaseProvider::getDb()->execute(
+				"ALTER TABLE `{$table}` ADD `{$field}` VARCHAR(16) NOT NULL default '' COMMENT 'tprice type (vt/tprice)';"
+			);
+		}
+
+		//clear oxarticle fields cache
+		$dir = oxRegistry::getConfig()->getConfigParam("sCompileDir") . "/*oxarticles*.txt";
+		foreach (glob($dir) as $item) if (!is_dir($item)) unlink($item);
+
+		// update views
+		$dbMetaDataHandler->updateViews();
+    }
+}
